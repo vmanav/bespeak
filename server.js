37980a74d9eda7c78c8f87886c55c7be34103dec
vmanav <zmanav.1999@gmail.com>
2020-08-31 01:22:57 +0530

    Booking and Update route setup

--- server.js
+++ server.js
@@ -9,22 +9,30 @@ app.use(express.urlencoded({ extended: true }));
 
 // Home 
 app.get('/', (req, res) => {
+
+
+  // let date = {
+  //   "day": 1,
+  //   "month": 9,
+  //   "year": 2020,
+  //   "hours": 20,
+  //   "minutes": 0
+  // };
+
+
+  // var schedule = new Date();
+  // schedule.setDate(parseInt(date.day));
+  // schedule.setMonth(parseInt(date.month));
+  // schedule.setFullYear(parseInt(date.year));
+  // schedule.setHours(parseInt(date.hours));
+  // schedule.setMinutes(parseInt(date.minutes));
+
+  // console.log("DATE :", schedule);
+
   res.send({
     "text": "Welcome to bespeak, Easy Ticket Booking",
     "status": 200
   })
-})
-
-// Get All Tickets
-app.post('/tickets/search', (req, res) => {
-
-  const timming = req.body.timming;
-  console.log("timming : ", timming);
-  res.send({
-    "text": timming,
-    "status": 200
-  })
-
 })
 
 // Ticket Booking
@@ -46,35 +54,22 @@ app.post('/tickets', (req, res) => {
   })
 })
 
-// View a Ticket
-
-
-// Update Ticket Timming 
-app.get('/tickets/:id', (req, res) => {
-
-  let id = req.params.id;
-  console.log("id : ", id);
-
-})
-
-
 // Update Ticket Timming 
 app.patch('/tickets/:id', (req, res) => {
 
   let id = req.params.id;
-  console.log("id : ", id);
+  let newTimming = req.body.timming;
+  res.send({
+    "id": id,
+    "newTimming": newTimming
+  })
 
+  Tickets.update({ timming: newTimming }, {
+    where: {
+      id: id
+    }
+  });
 })
-
-// Delete Ticket
-app.delete('/tickets/:id', (req, res) => {
-
-  let id = req.params.id;
-  console.log("id : ", id);
-
-})
-
-
 
 db.sync().then(() => {
   app.listen(3000, () => {
